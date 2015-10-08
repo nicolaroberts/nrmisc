569176f2887460b413bc052ce6b07e3e8efe6490
Nicola Roberts <nr3@sanger.ac.uk>
2015-10-08 09:29:18 +0100

    plot_gr_track func

--- R/granges_tools.R
+++ R/granges_tools.R
@@ -80,3 +80,47 @@ sample_GRanges <- function(gr, n){
     ans <- sort(ans)
     return(ans)
 }
+
+
+
+
+#' Plot one quantitative meta-data track from a GRanges object
+#'
+#' @param gr A GRanges object
+#' @param track Name of quantitative meta-data track to plot
+#' @param seqname seqname to plot for
+#' @param start starting position of plot (start of seqname if left NULL)
+#' @param end end position of plot (end of seqname if left NULL)
+#' @import GenomicRanges ggplot2
+#' @export
+plot_gr_track <- function(gr, track, seqname, start=NULL, end=NULL){
+
+    # set start and end if null
+    if (is.null(start)) start <- start(range(gr[seqnames(gr)==seqname]))
+    if (is.null(end)) end <- end(range(gr[seqnames(gr)==seqname]))
+
+    # subset gr
+    gr <- subsetByOverlaps(gr, GRanges(seqname, IRanges(start, end)))
+
+    # get info needed
+    mdata <- as.data.frame(gr)
+    pdata <- data.frame(seqnames=rep(mdata$seqnames, each=2),
+                        pos=interleave(mdata$start, mdata$end),
+                        value=rep(mdata[[track]], each=2))
+    # plot basics
+    p <- ggplot(data=pdata, aes(x=pos, y=value)) +
+        theme(panel.grid=element_blank(), text=element_text(size=16)) +
+        scale_x_continuous(name=paste('position on', seqname)) +
+        scale_y_continuous(name=track)
+
+    # plot line if number of ranges less than 2000, otherwise stat_bin2d
+    if (length(gr) <= 2000){
+        ans <- p + geom_line()
+    } else {
+        ans <- p + stat_bin2d(binwidth=c(width(range(gr))/1000,
+                                         diff(range(pdata$value))/100)) +
+            scale_fill_continuous(guide=FALSE)
+    }
+
+    return(ans)
+}
