063012acfe58a242de07e3341a8c0470e0dcd86e
Nicola Roberts <nr3@sanger.ac.uk>
2016-10-28 13:15:05 +0100

    data descriptions

--- R/nrmisc.R
+++ R/nrmisc.R
@@ -6,3 +6,18 @@
 #' @docType package
 #' @name nrmisc
 NULL
+
+#' Immune loci genes (hg19)
+#'
+#' @format GRanges
+"immune"
+
+#' Protein-coding transcript boundaries (hg19)
+#'
+#' @format GRanges
+"txs"
+
+#' Exon boundaries (hg19)
+#'
+#' @format GRanges
+"exs"
